630beaab89b327c3cf540521805fe7079c1b954b
ShahilMd <shahil.connect@gmail.com>
2025-11-24 14:35:22 +0530

    adding production frontend url

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import {createServer} from 'node:http';
 import {Server} from "socket.io";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 const  app = express()
 const server = createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:'http://localhost:5173',
+        origin:process.env.FRONTEND_URL,
     }
 });
 const ROOM = 'group'
